9284fcd8fea9f20204c2b5ce594c966d4e571b44
thinhlucius41 <thinhlucius41@gmail.com>
2020-06-12 18:45:12 +0700

    Database

---
+++ LTWeb/Controllers/TinTucController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.EF;
+
+namespace LTWeb.Controllers
+{
+    public class TinTucController : Controller
+    {
+        // GET: TinTuc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FirstNews()
+        {
+            LTWebDataContext db = new LTWebDataContext();
+            var d = from m in db.News
+                    where m.hide == true
+                    orderby m.IDnews descending
+                    select m;
+            return PartialView(d.First());
+        }
+    }
+}
